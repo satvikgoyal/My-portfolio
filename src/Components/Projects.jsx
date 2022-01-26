d6f3c7d093a86c5f380eae8d25c18f992422cca1
Satvik <svsgoyal@gmail.com>
2022-01-26 20:03:17 +0530

    change in slide show time

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -35,7 +35,7 @@ const Projects = () => {
         slidesToScroll: 1,
         autoplay: true,
         // speed: 3000,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 5000,
         cssEase: "linear",
         pauseOnHover: true,
         swipeToSlide: true,
